b96b6ab10b356589e93f6cea0dfebf0899993368
Kyle Mistele <kyle@mistele.com>
2023-08-11 11:28:30 -0500

    more resources

--- src/pages/claim/index.tsx
+++ src/pages/claim/index.tsx
@@ -4,6 +4,9 @@ import {Inter, Space_Grotesk} from 'next/font/google'
 import styles from './styles.module.scss'
 import Link from "next/link";
 import ellipse969 from "@/assets/img/ellipse-969.svg";
+import smooth1 from '@/assets/img/smooth-1.png'
+import rightArrow from "@/assets/img/right-arrow.svg";
+import flag from '@/assets/img/Icon/Feedback Flag.svg'
 
 const spaceGrotesk = Space_Grotesk({subsets: ['latin']})
 
@@ -24,8 +27,23 @@ export function Claim({}) {
 
 
                     <div className={styles.main__header}>
-                        <p className={styles.main__header__text} style={spaceGrotesk.style}> CLAIM YOUR NFT NOW</p>
+                        <p className={styles.main__header__text} style={spaceGrotesk.style}> Claim your NFT Now</p>
+                    </div>
 
+                    <Image src={smooth1} alt={''} className={styles.main__image}/>
+
+                    <div className={styles.claimGroup}>
+                        <input
+                            type={'email'}
+                            placeholder={'Enter your email address'}
+                            className={styles.claimGroup__input}
+                            style={spaceGrotesk.style}
+                        />
+
+                        <div className={styles.claimGroup__button}>
+                            <div className={styles.claimGroup__button__text} style={spaceGrotesk.style}>Claim Now</div>
+                            <Image src={flag} alt={''} className={styles.claimGroup__button__icon}/>
+                        </div>
                     </div>
 
 
